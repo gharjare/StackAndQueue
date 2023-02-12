db17fa3c5cacf41d273465ace948a9abf2619ee4
gharjare <gharjarechetan@gmail.com>
2023-02-12 17:39:43 +0530

    [chetan gharjare] create a stack

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace StackAndQueue
+﻿using StackAndQueue1;
+
+namespace StackAndQueue
 {
     class program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to stack and queue program");
+            Stack stack = new Stack();
+            stack.Insert(70);
+            stack.Insert(30);
+            stack.Insert(56);
+            stack.Display();
         }
     }
+    
 }
---
+++ Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue1
+{
+    public class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} inserted to stack", value);
+        }
+        
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
