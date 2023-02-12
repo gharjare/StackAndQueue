656327a5e0e3edecbe50689c57c352c4b1612522
gharjare <gharjarechetan@gmail.com>
2023-02-12 17:33:26 +0530

    [chetan gharjare] initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueue
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack and queue program");
+        }
+    }
+}
